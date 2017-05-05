2beb0f0337730e7fd16822a5bf08ad03ab83a319
Your Name <you@example.com>
2017-05-05 23:45:16 +0800

    test

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import pexpect
-
-child = pexpect.spawn ('telnet 52.79.200.113 32773')
+import sys
+import time
+child = pexpect.spawn('telnet 52.79.200.113 32773')
 child.logfile = sys.stdout
-child.expect(">")
-child.sendline("en")
+child.expect("#")
+child.sendline("show ip int brief")
+child.expect("#")
+child.sendline("exit")
+child.expect("#")
