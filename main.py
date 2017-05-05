bf214f466e9818124ebba6780138068190eb9f01
Your Name <you@example.com>
2017-05-05 22:29:33 +0800

    Test

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ hostname2 = "192.168.1.1"
 response = os.system("ping -c 1 " + hostname1)
 if response == 0:
      host1_stat = "Reachable"
- else:
+else:
      host1_stat = "Unreachable"
 
 
 response = os.system("ping -c 1 " + hostname2)
 if response == 0:
      host2_stat = "Reachable"
- else:
+else:
      host2_stat = "Unreachable"
+
+print("Host 1: {}".format(host1_stat))
+print("Host 2: {}".format(host2_stat))
