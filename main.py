9d3d262ac5729134bd45269f6a30e74f12988047
Your Name <you@example.com>
2017-05-05 22:17:57 +0800

    Ping Single Host

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+hostname = "google.com"
+response = os.system("ping -c 1 " + hostname)
+# if response == 0:
+#     pingstatus = "Network Active"
+# else:
+#     pingstatus = "Network Error"
