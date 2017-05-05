570f70d250ed1b149dc708a8f322b64c6c96e201
Your Name <you@example.com>
2017-05-05 22:27:21 +0800

    2 task

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
-hostname = "google.com"
-response = os.system("ping -c 1 " + hostname)
-# if response == 0:
-#     pingstatus = "Network Active"
-# else:
-#     pingstatus = "Network Error"
+hostname1 = "google.com"
+host1_stat = ""
+host2_stat = ""
+hostname2 = "192.168.1.1"
+
+response = os.system("ping -c 1 " + hostname1)
+if response == 0:
+     host1_stat = "Reachable"
+ else:
+     host1_stat = "Unreachable"
+
+
+response = os.system("ping -c 1 " + hostname2)
+if response == 0:
+     host2_stat = "Reachable"
+ else:
+     host2_stat = "Unreachable"
